647dfc6017d6fb377dbbbffdae394aa6d27ff0d2
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-23 21:21:05 +0200

    USER CRUD operation is added

--- src/image-manager/controller/user.js
+++ src/image-manager/controller/user.js
@@ -31,15 +31,50 @@ exports.signin = async (req, res, next) => {
 exports.gard = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, JWT_SECRET, (err, data) => {
+            if (err) return res.status(403).json({
+                ok: false, message: err.message,
+            });
+            req.user_id = data._id;
+            next();
+        });
     } catch (e) {
         return res.status(403).json({
             ok: false, message: e.message,
         })
     };
-    jwt.verify(token, JWT_SECRET, (err, data) => {
-        if (err) return res.status(403).json({
-            ok: false, message: err.message,
-        });
-        req.user_id = data._id;
+};
+
+
+exports.addUser = async (req, res, next) => {
+    const body = req.body;
+    try {
+        let user = await userModel.create(body);
+        res.status(200).json({ok: true, user_id: user._id})
+    } catch (e) {
+        res.status(500).json({ok: false, message: e.message,})
+    }
+};
+
+exports.updateUser = async(req, res, next)=>{
+    const userId = req.user_id; 
+    const user = await userModel.findOne({
+        _id: userId},{__v:0});
+    Object.assign(user, req.body); 
+    try{user.save();}catch(e){res.status(500).json({
+        ok:false, message: e.message,
+    })}
+    res.status(200).json({
+        ok:true, message:"user updated",
+    })
+};
+
+exports.deleteUser = async(req, res, next)=>{
+    const userId = req.user_id; 
+    userModel.deleteOne({
+        _id : userId, 
+    })
+    res.status(200).json({
+        ok:true, message:"user deleted",
     });
 }
