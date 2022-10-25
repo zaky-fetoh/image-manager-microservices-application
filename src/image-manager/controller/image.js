4f3b1e918250dda72e8e10f8ed3af752102f4def
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-25 23:01:55 +0200

    view-image route is deved

--- src/image-manager/controller/image.js
+++ src/image-manager/controller/image.js
@@ -10,32 +10,32 @@ const IS_PORT = process.env.IMAGEDISK_PORT;
 
 // "/image/:encrypt"
 exports.addImage = async (req, res, next) => {
-    const encrypt = req.params.encrypt==="true";
-    const path = (encrypt? "/cipher": "/plain") + "-image";
+    const encrypt = req.params.encrypt === "true";
+    const path = (encrypt ? "/cipher" : "/plain") + "-image";
     const user_id = req.user_id;
 
     const data = [];
 
     const imreq = http.request({
         host: IS_HOST, port: IS_PORT, path: path,
-        method:"POST", headers: req.headers, 
-    }, imres=>{
-        imres.on("data",chunk=> data.push(chunk)); 
-        imres.on("error",console.error);
-        imres.on("end",async()=>{
+        method: "POST", headers: req.headers,
+    }, imres => {
+        imres.on("data", chunk => data.push(chunk));
+        imres.on("error", console.error);
+        imres.on("end", async () => {
             const jdata = JSON.parse(Buffer.concat(data).toString());
             console.log(jdata);
-            if(jdata.ok){
+            if (jdata.ok) {
                 const im = await imageModel.create({
                     image_id: jdata.imageName,
                     owner: user_id,
-                    stored_incrypted:encrypt,
-                }); 
+                    stored_incrypted: encrypt,
+                });
                 return res.status(200).json({
                     image_id: im._id,
-                    ok: true, 
+                    ok: true,
                 })
-            }else{
+            } else {
                 return res.status(500).json({
                     ok: false,
                 })
@@ -46,43 +46,72 @@ exports.addImage = async (req, res, next) => {
 };
 
 // /image/:imageId
-exports.deleteImage = async(req, res, next)=>{
-    const user_id = req.user_id; 
-    const image_id= req.params.imageId;
+exports.deleteImage = async (req, res, next) => {
+    const user_id = req.user_id;
+    const image_id = req.params.imageId;
     const imDoc = await imageModel.findOne({
-        _id:image_id, owner: user_id,
+        _id: image_id, owner: user_id,
     });
 
-    if(!imDoc) return res.status(404).json({
-        ok:false, message: "File not found",
+    if (!imDoc) return res.status(404).json({
+        ok: false, message: "File not found",
     })
 
     const encrypt = imDoc.stored_incrypted === "true";
-    const path = (encrypt? "/cipher": "/plain") +
-                 "-image/"+imDoc.image_id;
+    const path = (encrypt ? "/cipher" : "/plain") +
+        "-image/" + imDoc.image_id;
     const data = []
     const imreq = http.request({
         host: IS_HOST, port: IS_PORT, path: path,
-        method:"DELETE", headers: req.headers, 
-    },dres=>{
-        dres.on("data",chunk=>data.push(chunk))
-        dres.on("error", err=>res.status(500).json({
-            ok:false, message: err.message,
+        method: "DELETE", headers: req.headers,
+    }, dres => {
+        dres.on("data", chunk => data.push(chunk))
+        dres.on("error", err => res.status(500).json({
+            ok: false, message: err.message,
         }));
-        dres.on("end",async()=>{
+        dres.on("end", async () => {
             const jdata = JSON.parse(Buffer.concat(data).toString());
-            if(jdata.ok){
+            if (jdata.ok) {
                 const result = await imageModel.deleteOne({
-                    _id:image_id, owner: user_id,
+                    _id: image_id, owner: user_id,
                 });
                 res.status(200).json({
-                    ok:true, deletedCount: result.deletedCount,
+                    ok: true, deletedCount: result.deletedCount,
                 })
-            }else{
+            } else {
                 res.status(500).json({
-                    ok:false, message: jdata.message,
+                    ok: false, message: jdata.message,
                 });
             }
         });
     }).end();
-}
+};
+
+
+// /view-image/:imageId GET
+exports.viewImage = async (req, res, next) => {
+    const user_id = req.user_id;
+    const imageId = req.params.imageId;
+
+    const imDoc = await imageModel.findOne({
+        image_id: imageId,
+    }, { __v: 0 });
+
+    if (!imDoc) return res.status(404).json({
+        ok: false, message: "image not found",
+    })
+    if (user_id !== imDoc.owner) {
+        imDoc.view_count++;
+        setImmediate(async() => await imDoc.save());
+    }
+    const encrypt = imDoc.stored_incrypted === "true";
+    const path = (encrypt ? "/cipher" : "/plain") +
+        "-image/" + imDoc.image_id;
+
+    http.get({
+        host: IS_HOST, port: IS_PORT,
+        path: path, headers: req.headers,
+    }, ires => {
+        ires.pipe(res)
+    }).end()
+};
