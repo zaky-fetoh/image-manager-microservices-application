6af25b60c0a5a3fac849a3b7681b9d10f48f8763
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-26 20:26:25 +0200

    image classification pakage is added, by requesting an image from IS and return results

---
+++ src/image-classification/controller/classification.py
@@ -0,0 +1,16 @@
+import requests, os, model
+import io, PIL.Image as Image
+
+IS_HOST = os.getenv("IMAGEDISK_HOST")
+IS_PORT = os.getenv("IMAGEDISK_PORT")
+
+def classifyImage_from_storage(imageId:str)->list:
+    url= "http://"+IS_HOST+":"+IS_PORT+\
+    "/plain-image/e416c158-1862-4670-b477-3e9fbdf0d0c1.jpeg"
+    res = requests.get(url,stream=True)
+    file = io.BytesIO(res.content)
+    im = Image.open(file)
+    pred = model.getPred(im)
+    return pred
+
+
--- src/image-classification/main.py
+++ src/image-classification/main.py
@@ -9,20 +9,6 @@ import os
 os.environ["IMAGEDISK_HOST"] = "localhost"
 os.environ["IMAGEDISK_PORT"] = "3000"
 
-IS_HOST = os.getenv("IMAGEDISK_HOST")
-IS_PORT = os.getenv("IMAGEDISK_PORT")
-
-url= "http://"+IS_HOST+":"+IS_PORT+\
-    "/plain-image/e416c158-1862-4670-b477-3e9fbdf0d0c1.jpeg"
-
-res = req.get(url,stream=True)
-
-
-file = io.BytesIO(res.content)
-
-im = Image.open(file)
-pred = model.getPred(im)
-
 print( pred ); 
 
 # flask.Flask(__name__).run(debug=True)
