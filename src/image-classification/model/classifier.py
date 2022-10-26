9cf7de6f2e35fa587880d8a06102962181550e2d
Zaky fetoh <zaky.fetoh@gmail.com>
2022-10-26 20:05:43 +0200

    classification pakage is added

---
+++ src/image-classification/model/classifier.py
@@ -0,0 +1,28 @@
+import torch.utils.data as data
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torch as t
+
+class_labels = eval(open("model/imageNetLabel.txt").read())
+
+# resnet = models.resnet18(pretrained=True)
+# resnet_script.save("rescript.pt");
+resnet_script = t.jit.load("rescript.pt")
+
+preprocess = transforms.Compose([
+    # transforms.ToPILImage,
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    lambda x: x[:3],
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225])
+])
+
+
+def getPred(im, top=4):
+    im = preprocess(im).unsqueeze_(0)
+    cls_pred = resnet_script(im)
+    _, inds = t.sort(cls_pred, dim=-1, descending=True)
+    return [class_labels[inds[0, i].item()]for i in range(top)]
