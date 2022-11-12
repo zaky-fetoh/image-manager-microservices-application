77a731d64b9412ff317534c8b35e8fb6a781f20c
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-12 19:46:39 +0200

    service-registry model is developed

---
+++ src/service-registry/model/service-model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+
+const ServiceSchema = new mongoose.Schema({
+    _id:{ type: mongoose.Schema.Types.ObjectId, 
+        default: mongoose.Types.ObjectId,
+    }, 
+    service_name:{
+        type: mongoose.Schema.Types.String,
+        required: true, 
+    },
+    service_version:{
+        type: mongoose.Schema.Types.String, 
+        required: true, 
+    },
+    hostname:{ 
+        type: mongoose.Schema.Types.String, 
+        required: true, 
+    }, 
+    port:{
+        type: mongoose.Schema.Types.Number, 
+        required: true, 
+    }
+});
+
+module.exports = new mongoose.model("service_registry", ServiceSchema); 
