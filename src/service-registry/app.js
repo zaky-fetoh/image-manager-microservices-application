b2353652cddf2222747329acbc65e381ef3d39fe
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-12 23:21:25 +0200

    service registry midelleware

--- src/service-registry/app.js
+++ src/service-registry/app.js
@@ -1,4 +1,19 @@
-
+const express = require("express");
 const mongoose = require("mongoose");
 mongoose.pluralize(null);
 
+(async () => {
+
+    console.log("Connecting to Database");
+    try {
+        await mongoose.connect(process.env.DB_URI);
+    } catch (e) {
+        console.error("Unable to connect To DB");
+        console.error(e.message);
+    }
+
+    express()
+
+
+
+})()
---
+++ src/service-registry/controller/service-meddleware.js
@@ -0,0 +1,53 @@
+const dbops = require("./service-database");
+
+exports.addService = async (req, res, next) => {
+    /****************************
+     * this Method adds the Services
+     * Route : POST /service/:name/:version/:port
+     * return: {ok:Boolean, service_id}
+     ******************************/
+    const service = {}
+    service.name = req.params.name;
+    service.version = req.params.version;
+    service.port = Number(req.params.port);
+    const ip = req.connection.remoteAddress;
+    service.hostname = ip.includes('::') ? `[${ip}]` : ip;
+    const out = await dbops.addService(service);
+    res.status(out.ok ? 200 : 500).json(out)
+};
+
+
+exports.deleteService = async (req, res, next) => {
+    /****************************
+     * this Method deletes the Services
+     * Route : DELETE /service/:name/:version/:port
+     * return: {ok:Boolean, service_id}
+     ******************************/
+    const service = {}
+    service.name = req.params.name;
+    service.version = req.params.version;
+    service.port = Number(req.params.port);
+    const ip = req.connection.remoteAddress;
+    service.hostname = ip.includes('::') ? `[${ip}]` : ip;
+    const out = await dbops.deleteService(service);
+    res.status(out.ok ? 200 : 500).json(out);
+};
+
+exports.get = async (req, res, next) => {
+    /****************************
+     * this Method gets the Services
+     * Route : GET /service/:name/:version
+     * return: {ok:Boolean, service_id}
+     ******************************/
+    const service = {};
+    service.name = req.params.name;
+    service.version = req.params.version;
+    const retServ = await dbops.getService(service);
+    if(retServ) return res.status(200).json({
+        ok: true, hostname: retServ.hostname,
+        port: retServ.port,
+    });
+    else res.status(500).json({
+        ok: false, message: "Not Service Found", 
+    });
+};
