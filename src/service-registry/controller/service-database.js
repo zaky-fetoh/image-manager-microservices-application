d67d33eba778514cbcd6efbfaf54f8b9a29f8ba9
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-13 07:46:37 +0200

    logs is added for debuging

--- src/service-registry/controller/service-database.js
+++ src/service-registry/controller/service-database.js
@@ -7,6 +7,7 @@ exports.addService = async (service_info) => {
      * input: service info {name, version, hostname, port}
      * return {ok: Boolean, service_id: ObjectId}
      ********************************************/
+    console.log(`adding services ${service_info.name}|${service_info.version}|${service_info.hostname}|${service_info.port}`)
     try {
         const doc = await serviceModel.create(service_info);
         return { ok: true, service_id: doc._id };
@@ -22,9 +23,10 @@ const RandInt = (n)=>{ // Generate Random NUMBer
 exports.getService=async(service_filter_info)=>{
         /********************************************
      * This method retrive a service from Database
-     * input: service info {name, version, hostname, port}
+     * input: service info {name, version}
      * return {ok: Boolean, hostname: String, Port:Number}
      ********************************************/
+    console.log(`Searching service ${JSON.stringify(service_filter_info)}`)
     const searchVersion = service_filter_info.version;
     delete service_filter_info.version;
     const services = await serviceModel.find(service_filter_info,
@@ -43,6 +45,7 @@ exports.deleteService = async (service_info) => {
      * input: service info {name, version, hostname, port}
      * return {ok: Boolean, deletedCount: ObjectId}
      ********************************************/
+    console.log(`Deleting service${JSON.stringify(service_info)}`)
     try {
         const dltDocs = await serviceModel.deleteMany(service_info);
         return { ok: true, deletedCount: dltDocs.deletedCount };
