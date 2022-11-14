d7458a4f32c1f6947a5390099baa5223ba20b2f8
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-14 12:56:53 +0200

    forward method create new request to the required microservice and pipes that incomming request to it

--- src/api-gateway/controller/service-discovery.js
+++ src/api-gateway/controller/service-discovery.js
@@ -6,6 +6,13 @@ const SR_PORT = process.env.SER_REG_PORT;
 discURL = `http://${SR_HOST}:${SR_PORT}/service`
 
 exports.getServiceURL = async (name, version) => {
+    /******************************
+     * this Method request the Service registry micro-service
+     * to acquire Host and port of the $name with corresponding
+     * $version. 
+     * INPUT: String, //name is the service name
+     * Return {ok:boolean, ... }
+     ******************************/
     try {
         const res = await axios.get(
             `${discURL}/${name}/${version}`);
