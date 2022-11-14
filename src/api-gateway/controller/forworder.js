09ea35fb74e2a7f7bf32a0f8616023191f8f7298
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-14 23:27:02 +0200

    refactor spacies

--- src/api-gateway/controller/forworder.js
+++ src/api-gateway/controller/forworder.js
@@ -1,12 +1,12 @@
 const http = require("http"); 
 const url = require("url");
 
-exports.forward = async({req, res},toEndPoint, method)=>{
+exports.forward = async(req, res,toEndPoint, method)=>{
     /************************
      * this method forword the incomming Request $req
      * to the $toEndPoint and it's response to the original
      * incomming response $res
-     * INPUT: {Req, res} object which is expected req, res
+     * INPUT: req, res object which is expected req, res
      * Express Objects. 
      * toEndPoint:string is the route for forwarding
      * to EndPoint is expectedto be of form of 
