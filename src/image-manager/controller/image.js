fc6e08efb0ee6314b1301fa22db8de23d5bfa36a
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-18 00:07:18 +0200

    image-manager no longer depend on the fixed Image-storage service it now get current IS address from SR

--- src/image-manager/controller/image.js
+++ src/image-manager/controller/image.js
@@ -2,13 +2,24 @@ const http = require("http");
 const amqp = require("amqplib");
 
 const imageModel = require("../model/image");
+const SD = require("./service-discovery");
 
-const IS_HOST = process.env.IMAGEDISK_HOST;
-const IS_PORT = process.env.IMAGEDISK_PORT;
+
+
+const getIS_addr = async () => {
+    const IS_addr = await SD.discover("image-storage", "1.0.0")
+    if (IS_addr) return {
+        IS_HOST: IS_addr.hostname,
+        IS_PORT: IS_addr.port
+    };else throw new Error('No Image Storage Found');
+}
 
 
 // "/image/:encrypt"
 exports.addImage = async (req, res, next) => {
+
+    const { IS_HOST, IS_PORT } = getIS_addr()
+
     const encrypt = req.params.encrypt === "true";
     const path = (encrypt ? "/cipher" : "/plain") + "-image";
     const user_id = req.user_id;
@@ -47,6 +58,9 @@ exports.addImage = async (req, res, next) => {
 
 // /image/:imageId
 exports.deleteImage = async (req, res, next) => {
+
+    const { IS_HOST, IS_PORT } = getIS_addr()
+
     const user_id = req.user_id;
     const image_id = req.params.imageId;
     const imDoc = await imageModel.findOne({
@@ -90,6 +104,7 @@ exports.deleteImage = async (req, res, next) => {
 
 // /view-image/:imageId GET
 exports.viewImage = async (req, res, next) => {
+    const { IS_HOST, IS_PORT } = getIS_addr()
     const user_id = req.user_id;
     const imageId = req.params.imageId;
 
@@ -102,7 +117,7 @@ exports.viewImage = async (req, res, next) => {
     })
     if (user_id !== imDoc.owner.toString()) {
         imDoc.view_count++;
-        setImmediate(async() => await imDoc.save());
+        setImmediate(async () => await imDoc.save());
     }
     const encrypt = imDoc.stored_incrypted === "true";
     const path = (encrypt ? "/cipher" : "/plain") +
@@ -117,27 +132,26 @@ exports.viewImage = async (req, res, next) => {
 };
 
 
-exports.predictTages = async(imageId)=>{
+exports.predictTages = async (imageId) => {
     const imDoc = await imageModel.findOne({
-        _id:imageId,
+        _id: imageId,
     })
-
     const conn = await amqp.connect(process.env.RABBITMQ);
     const channel = await conn.createChannel();
     const exch = await channel.assertExchange(process.env.MQ_EXCHANGE,
-                "direct",{ durable:false});
-    const resq = await channel.assertQueue("",{
-        durable:true,
+        "direct", { durable: false });
+    const resq = await channel.assertQueue("", {
+        durable: true,
     });
-    channel.publish(exch.exchange,"resnet",
-                    Buffer.from(imDoc.image_id+" XX "+ imDoc.stored_incrypted),{
-                        correlationId:imageId,
-                        replyTo: resq.queue,
-                    });
-    await channel.consume(resq.queue,async(msg)=>{
+    channel.publish(exch.exchange, "resnet",
+        Buffer.from(imDoc.image_id + " XX " + imDoc.stored_incrypted), {
+        correlationId: imageId,
+        replyTo: resq.queue,
+    });
+    await channel.consume(resq.queue, async (msg) => {
         body = JSON.parse(msg.content.toString());
         imDoc.tags = body.labels;
         await imDoc.save()
     })
-    
+
 }
